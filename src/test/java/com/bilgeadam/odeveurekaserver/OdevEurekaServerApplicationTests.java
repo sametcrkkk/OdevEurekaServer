e7f6b6151e4fceb13c42843314ee5f269be66358
Samet <sametcoruk0604@gmail.com>
2022-02-12 13:21:51 +0300

    Eureka client.

---
+++ src/test/java/com/bilgeadam/odeveurekaserver/OdevEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.odeveurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdevEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
